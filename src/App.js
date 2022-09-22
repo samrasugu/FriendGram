b291a35142176dda778156f66441e50c0ac6cf05
samrasugu <mokuasamr@gmail.com>
2022-09-22 23:36:41 +0300

    Auth Context API

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 import {
   BrowserRouter,
+  Navigate,
   Route,
   Routes,
 } from "react-router-dom";
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import { createTheme, ThemeProvider } from '@mui/material';
+import { AuthContext } from './context/AuthContext';
 
 function App() {
 
@@ -20,13 +22,24 @@ function App() {
     },
   });
 
+  const {currentUser} = useContext(AuthContext);
+
+  const ProtectedRoute =({children}) => {
+    if (!currentUser) {
+      return <Navigate to='/login'/>
+    }
+    return children
+  }
+
   return (
     <ThemeProvider theme={darkTheme}>
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home  setMode={setMode} mode={mode}/>}/>
-        <Route path='/login' element={<Login/>}/>
-        <Route path='/signup' element={<Signup/>}/>
+        <Route path="/">
+          <Route index element={<ProtectedRoute><Home setMode={setMode} mode={mode}/></ProtectedRoute>}/>
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/signup' element={<Signup/>}/>
+        </Route>
       </Routes>
     </BrowserRouter>
     </ThemeProvider>
