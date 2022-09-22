3e4448b69ca65c0135350f81f2702faf1b3800f5
samrasugu <mokuasamr@gmail.com>
2022-09-22 15:43:01 +0300

    Login & Signup basic UI

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,18 @@
+import { Box, Button, TextField, Typography } from '@mui/material'
 import React from 'react'
+import {Link} from 'react-router-dom';
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <Box sx={{display: "flex", alignItems: "center", justifyContent: "center", height: "100vh"}}>
+        <Box sx={{display: "flex", flexDirection: "column", alignItems: "center", margin: "30px"}}>
+            <Typography variant="h1" component="h2" sx={{fontSize: "30px"}}>FriendGram</Typography>
+            <TextField label="Email" size='small' variant='outlined' sx={{margin: '10px' }}/>
+            <TextField label="Password" size='small' variant='outlined' sx={{margin: '10px' }}/>
+            <Button variant='contained' sx={{margin: "10px"}}>SIGN IN</Button>
+            <Typography>New here? <Link to="/signup">Register</Link></Typography>
+        </Box>
+    </Box>
   )
 }
 
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import {Link} from 'react-router-dom';
+import { Box, Button, TextField, Typography } from '@mui/material'
 
 const Signup = () => {
   return (
-    <div>Signup</div>
+    <Box sx={{display: "flex", alignItems: "center", justifyContent: "center", height: "100vh"}}>
+        <Box sx={{display: "flex", flexDirection: "column", alignItems: "center", margin: "30px"}}>
+            <Typography variant="h1" component="h2" sx={{fontSize: "30px"}}>FriendGram</Typography>
+            <TextField label="First Name" size='small' variant='outlined' sx={{margin: '10px' }}/>
+            <TextField label="Last Name" size='small' variant='outlined' sx={{margin: '10px' }}/>
+            <TextField label="Email" size='small' variant='outlined' sx={{margin: '10px' }}/>
+            <TextField label="Password" size='small' variant='outlined' sx={{margin: '10px' }}/>
+            <Button variant='contained' sx={{margin: "10px"}}>SIGN UP</Button>
+            <Typography>Have an account? <Link to="/login">Login</Link></Typography>
+        </Box>
+    </Box>
   )
 }
 
